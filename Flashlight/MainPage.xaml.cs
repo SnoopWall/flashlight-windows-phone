b49a907fc01d3832e324c1d20e34b1f210d2065b
Chris Gauthier <chris.gauthier@snoopwall.com>
2014-10-09 08:46:53 -0400

    Adds license information.

--- Flashlight/MainPage.xaml.cs
+++ Flashlight/MainPage.xaml.cs
@@ -1,4 +1,20 @@
-﻿using System;
+﻿/*
+The Likeness, Logos and text of this app are provided as reference and may not be reproduced in a derivative work
+Copyright 2014 SnoopWall LLC
+
+        Licensed under the Apache License, Version 2.0 (the "License");
+        you may not use this file except in compliance with the License.
+        You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+        Unless required by applicable law or agreed to in writing, software
+        distributed under the License is distributed on an "AS IS" BASIS,
+        WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+        See the License for the specific language governing permissions and
+        limitations under the License.
+*/
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -26,7 +42,7 @@ namespace Flashlight {
         // Constructor
         public MainPage() {
             InitializeComponent();
-
+            // retrieve button images
             ToggleFlash.ApplyTemplate();
             flashImage = FindVisualChildByType<Image>(ToggleFlash, "ToggleFlashImage");
             ToggleScreen.ApplyTemplate();
@@ -35,6 +51,7 @@ namespace Flashlight {
 
         protected override void OnNavigatedTo(NavigationEventArgs e) {
             base.OnNavigatedTo(e);
+            // reset flashlight status to off
             InitializeCaptureDevice();
             flashOn = false;
             flashImage.Source = App.Current.Resources["flash"] as BitmapImage;
@@ -59,6 +76,7 @@ namespace Flashlight {
         }
 
         private void Website_Tap(object sender, System.Windows.Input.GestureEventArgs e) {
+            // launch browser with snoopwall website
             WebBrowserTask webBrowserTask = new WebBrowserTask();
             webBrowserTask.Uri = new Uri("http://www.snoopwall.com", UriKind.Absolute);
             webBrowserTask.Show();
@@ -92,6 +110,7 @@ namespace Flashlight {
             screenOn = !screenOn;
         }
 
+        // load reference to child object
         static T FindVisualChildByType<T>(DependencyObject element, String name) where T : DependencyObject {
             if (element is T && (element as FrameworkElement).Name == name)
                 return element as T;
